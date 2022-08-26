cdeb399acdf90a844853f1293dce421b572ccd9d
Kaito Nakashima <kaitotti0729@gmail.com>
2022-08-26 17:50:10 +0900

    Update

--- MogiManager/Global.py
+++ MogiManager/Global.py
@@ -1,3 +1,6 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+def DrawGrid(frame, width, height):
+    for i in range(width)
--- MogiManager/Wakeup.py
+++ MogiManager/Wakeup.py
@@ -1,6 +1,8 @@
 #営業設定画面
 
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkcalendar as tkc
 import Global as G
 
 FrmWakeup = tk.Frame(G.root, width=960, height=540)
@@ -8,3 +10,8 @@ FrmWakeup = tk.Frame(G.root, width=960, height=540)
 LblTitle = tk.Label(FrmWakeup, text="営業設定", font=("", 45))
 LblTitle.place(y=0, x=0)
 
+LblOpenDate = tk.Label(FrmWakeup, text="営業日", font=("", 25))
+LblOpenDate.place(y=100, x=100)
+
+DpOpenDate = tkc.DateEntry(master=FrmWakeup, showweeknumbers=False, state="readonly", font=("", 20))
+DpOpenDate.place(y=100, x=200)
